0f1aa3fa18d67eb3d2a18d9001d32dadbfe5d020
Ashwin <h1210093@nushigh.edu.sg>
2018-12-18 15:44:24 +0800

    Added admin account

--- define.sql
+++ define.sql
@@ -4,9 +4,18 @@ create table app_user(
 	name text NOT NULL
 );
 
-INSERT INTO appuser (username, passwordHash, name) VALUES
+create table app_admin(
+	username text PRIMARY KEY NOT NULL,
+	passwordHash text NOT NULL,
+	name text NOT NULL
+);
+
+INSERT INTO app_user (username, passwordHash, name) VALUES
 	('ME5Bob', '$2a$05$xuLyzrAaW7Y4mXGAXzPjOOIdTim2BVGzePV73H.Vsy8gKCUxXqRB2', 'Robert Lim'), --DarkHorseBigCat
 	('ME6Alice', '$2a$05$KlEAswrxBhrgXr1Fh7Io0ecBHB182FeJAuP8BppK8BVRujEPlavkK', 'Alice Ng'), --abcdef12345!@#$%
 	('safosscholar', '$2a$05$a6TJMr8YD4jUese5NKOL0.bDNvNMgV5.aDEhWpomDCYtz6jLn7GNe', 'Jonathan Tan'), --pas5W0rD!!!
 	('AirForceMan', '$2a$05$hAzW2PGlX4ig/oUChUxyA.1R0QqrKMCuxnlifS9qFbQQCivy6OvSq', 'Benjamin Net'), --WhyNotUseAnEasyPasswordLikeThis?
 	('TestUser', '$2a$05$D/nbFy9utEDFgg.Jfsl39epqO2Yx2nIRClYFGVMw9fnLZlXFFnP5u', 'User McUserson'); --WhatAreDictionaryAttacks
+
+INSERT into app_admin(username, passwordHash, name) VALUES 
+	('Hackerman','$2a$05$YNWHk.7Su/St644J1BAX7.G8KP3t5ts16bcAPApXSw.yc4hHrgwNi','Drop Table')
