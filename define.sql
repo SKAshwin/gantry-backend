0090e1be041013e68557d9a0f05b3743af714520
Ashwin <h1210093@nushigh.edu.sg>
2018-12-18 11:29:11 +0800

    Created the sample usernames for testing

--- define.sql
+++ define.sql
@@ -1,6 +1,12 @@
-create table "user"(
+create table app_user(
 	username text PRIMARY KEY NOT NULL,
 	passwordHash text NOT NULL,
 	name text NOT NULL
 );
 
+INSERT INTO appuser (username, passwordHash, name) VALUES
+	('ME5Bob', '$2a$05$xuLyzrAaW7Y4mXGAXzPjOOIdTim2BVGzePV73H.Vsy8gKCUxXqRB2', 'Robert Lim'), --DarkHorseBigCat
+	('ME6Alice', '$2a$05$KlEAswrxBhrgXr1Fh7Io0ecBHB182FeJAuP8BppK8BVRujEPlavkK', 'Alice Ng'), --abcdef12345!@#$%
+	('safosscholar', '$2a$05$a6TJMr8YD4jUese5NKOL0.bDNvNMgV5.aDEhWpomDCYtz6jLn7GNe', 'Jonathan Tan'), --pas5W0rD!!!
+	('AirForceMan', '$2a$05$hAzW2PGlX4ig/oUChUxyA.1R0QqrKMCuxnlifS9qFbQQCivy6OvSq', 'Benjamin Net'), --WhyNotUseAnEasyPasswordLikeThis?
+	('TestUser', '$2a$05$D/nbFy9utEDFgg.Jfsl39epqO2Yx2nIRClYFGVMw9fnLZlXFFnP5u', 'User McUserson'); --WhatAreDictionaryAttacks
