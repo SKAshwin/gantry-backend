5d8daa7a229a8579106543833eb349f1dafbfbbd
Ashwin <h1210093@nushigh.edu.sg>
2018-12-18 15:44:14 +0800

    Database initialization and error handling middleware

--- main.go
+++ main.go
@@ -2,30 +2,134 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/rs/cors"
 	"golang.org/x/crypto/bcrypt"
 )
 
-var HASH_COST = 5 //cost must be above 4, the larger you make it the slower the hash function will run
-var ALLOWED_CORS_ORIGINS = []string{"http://localhost:8080"}
-var LOGIN_URL = "/api/auth/login"
+var hashCost = 5 //cost must be above 4, the larger you make it the slower the hash function will run
+var allowedCorsOrigins = []string{"http://localhost:8080"}
+var loginURL = "/api/auth/login"
 var signingKey = []byte("theSecretPassword")
-var LOGIN_TOKEN_USERNAME, LOGIN_TOKEN_EXPIRY_TIME = "username", "exp"
+var loginTokenUsername, loginTokenExpiryTime = "username", "exp"
+
+const (
+	dbhost = "DBHOST"
+	dbport = "DBPORT"
+	dbuser = "DBUSER"
+	dbpass = "DBPASS"
+	dbname = "DBNAME"
+)
+
+var db *sql.DB
 
 func main() {
+	//redirectLogger()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		panic(err)
+	}
+	initDB()
 	r := mux.NewRouter()
-	r.Handle(LOGIN_URL, loginHandler).Methods("POST")
+	r.Handle(loginURL, recoverWrap(loginHandler)).Methods("POST")
 	handler := cors.New(cors.Options{
-		AllowedOrigins: ALLOWED_CORS_ORIGINS,
+		AllowedOrigins: allowedCorsOrigins,
 	}).Handler(r) //only allow GETs POSTs from that address (LOGIN_URL, the client-side address); the bare minimum needed
 	http.ListenAndServe(":3000", handler) //PostGres listens on 5432
+}
+
+func redirectLogger() {
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	log.SetOutput(file)
+}
+
+func initDB() {
+	config := dbConfig()
+	var err error
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		config[dbhost], config[dbport],
+		config[dbuser], config[dbpass], config[dbname])
+
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("Could not open databse")
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Could not ping database")
+	}
+	log.Println("Successfully connected!")
+}
+
+func dbConfig() map[string]string {
+	conf := make(map[string]string)
+	host, ok := os.LookupEnv(dbhost)
+	if !ok {
+		panic("DBHOST environment variable required but not set")
+	}
+	port, ok := os.LookupEnv(dbport)
+	if !ok {
+		panic("DBPORT environment variable required but not set")
+	}
+	user, ok := os.LookupEnv(dbuser)
+	if !ok {
+		panic("DBUSER environment variable required but not set")
+	}
+	password, ok := os.LookupEnv(dbpass)
+	if !ok {
+		panic("DBPASS environment variable required but not set")
+	}
+	name, ok := os.LookupEnv(dbname)
+	if !ok {
+		panic("DBNAME environment variable required but not set")
+	}
+	conf[dbhost] = host
+	conf[dbport] = port
+	conf[dbuser] = user
+	conf[dbpass] = password
+	conf[dbname] = name
+	return conf
+}
+
+func recoverWrap(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		defer func() {
+			r := recover()
+			if r != nil {
+				switch t := r.(type) {
+				case string:
+					err = errors.New(t)
+				case error:
+					err = t
+				default:
+					err = errors.New("Unknown error")
+				}
+				log.Println(err.Error())
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r)
+	})
 }
 
 type loginDetails struct {
@@ -42,25 +146,18 @@ var loginHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)
 	var ld loginDetails
 	err := decoder.Decode(&ld)
 	if err != nil {
-		fmt.Println(err)
 		panic(err)
 	}
 	fmt.Println(ld.Username)
 
 	if authenticate(ld) {
 		jwtToken := createToken(ld)
-		if reply, err := json.Marshal(map[string]string{"accessToken": jwtToken}); err != nil {
-			panic(err) //TODO deal with json marshalling error
-		} else {
-			w.Write(reply)
-		}
+		reply, _ := json.Marshal(map[string]string{"accessToken": jwtToken})
+		w.Write(reply)
 	} else {
 		w.WriteHeader(http.StatusUnauthorized)
-		if reply, err := json.Marshal(response{Message: "Incorrect Username or Password"}); err != nil {
-			panic(err) //TODO deal with json marshalling error
-		} else {
-			w.Write(reply)
-		}
+		reply, _ := json.Marshal(response{Message: "Incorrect Username or Password"})
+		w.Write(reply)
 	}
 
 })
@@ -69,10 +166,13 @@ func createToken(user loginDetails) string {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
 
-	claims[LOGIN_TOKEN_USERNAME] = user.Username
-	claims[LOGIN_TOKEN_EXPIRY_TIME] = time.Now().Add(time.Hour).Unix()
+	claims[loginTokenUsername] = user.Username
+	claims[loginTokenExpiryTime] = time.Now().Add(time.Hour).Unix()
 
-	tokenSigned, _ := token.SignedString(signingKey)
+	tokenSigned, err := token.SignedString(signingKey)
+	if err != nil {
+		panic(err)
+	}
 
 	return tokenSigned
 }
@@ -89,7 +189,7 @@ func authenticate(user loginDetails) bool {
 func hashAndSalt(pwd []byte) string {
 	// Use GenerateFromPassword to hash & salt pwd.
 	//cost must be above 4
-	hash, err := bcrypt.GenerateFromPassword(pwd, HASH_COST)
+	hash, err := bcrypt.GenerateFromPassword(pwd, hashCost)
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
@@ -99,6 +199,6 @@ func hashAndSalt(pwd []byte) string {
 	return string(hash)
 }
 
-var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+var notImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Not Implemented")
 })
