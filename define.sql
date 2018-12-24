55550f291a2a99daf026c74b52d3133be2542f59
Ashwin <h1210093@nushigh.edu.sg>
2018-12-24 11:56:56 +0800

    Implemented /api/app/users.

--- define.sql
+++ define.sql
@@ -4,7 +4,10 @@ create DATABASE registrationapp;
 create table app_user(
 	username text PRIMARY KEY NOT NULL,
 	passwordHash text NOT NULL,
-	name text NOT NULL
+	name text NOT NULL,
+	createdAt TIMESTAMP NOT NULL,
+	updatedAt TIMESTAMP NOT NULL,
+	lastLoggedIn TIMESTAMP NOT NULL
 );
 
 create table app_admin(
@@ -27,12 +30,12 @@ create table hosts(
 	PRIMARY KEY(username, eventID)
 );
 
-INSERT INTO app_user (username, passwordHash, name) VALUES
-	('ME5Bob', '$2a$05$xuLyzrAaW7Y4mXGAXzPjOOIdTim2BVGzePV73H.Vsy8gKCUxXqRB2', 'Robert Lim'), --DarkHorseBigCat
-	('ME6Alice', '$2a$05$KlEAswrxBhrgXr1Fh7Io0ecBHB182FeJAuP8BppK8BVRujEPlavkK', 'Alice Ng'), --abcdef12345!@#$%
-	('safosscholar', '$2a$05$a6TJMr8YD4jUese5NKOL0.bDNvNMgV5.aDEhWpomDCYtz6jLn7GNe', 'Jonathan Tan'), --pas5W0rD!!!
-	('AirForceMan', '$2a$05$hAzW2PGlX4ig/oUChUxyA.1R0QqrKMCuxnlifS9qFbQQCivy6OvSq', 'Benjamin Net'), --WhyNotUseAnEasyPasswordLikeThis?
-	('TestUser', '$2a$05$D/nbFy9utEDFgg.Jfsl39epqO2Yx2nIRClYFGVMw9fnLZlXFFnP5u', 'User McUserson'); --WhatAreDictionaryAttacks
+INSERT INTO app_user (username, passwordHash, name, createdAt, updatedAt, lastLoggedIn) VALUES
+	('ME5Bob', '$2a$05$xuLyzrAaW7Y4mXGAXzPjOOIdTim2BVGzePV73H.Vsy8gKCUxXqRB2', 'Robert Lim', NOW(), NOW(), NOW()), --DarkHorseBigCat
+	('ME6Alice', '$2a$05$KlEAswrxBhrgXr1Fh7Io0ecBHB182FeJAuP8BppK8BVRujEPlavkK', 'Alice Ng', NOW(), NOW(), NOW()), --abcdef12345!@#$%
+	('safosscholar', '$2a$05$a6TJMr8YD4jUese5NKOL0.bDNvNMgV5.aDEhWpomDCYtz6jLn7GNe', 'Jonathan Tan', NOW(), NOW(), NOW()), --pas5W0rD!!!
+	('AirForceMan', '$2a$05$hAzW2PGlX4ig/oUChUxyA.1R0QqrKMCuxnlifS9qFbQQCivy6OvSq', 'Benjamin Net', NOW(), NOW(), NOW()), --WhyNotUseAnEasyPasswordLikeThis?
+	('TestUser', '$2a$05$D/nbFy9utEDFgg.Jfsl39epqO2Yx2nIRClYFGVMw9fnLZlXFFnP5u', 'User McUserson', NOW(), NOW(), NOW()); --WhatAreDictionaryAttacks
 
 INSERT into app_admin(username, passwordHash, name) VALUES 
 	('Hackerman','$2a$05$YNWHk.7Su/St644J1BAX7.G8KP3t5ts16bcAPApXSw.yc4hHrgwNi','Drop Table');
--- users.go
+++ users.go
@@ -1,9 +1,62 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
+	"time"
 )
 
-var listUsersHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+type userPublicDetail struct {
+	Username     string
+	Name         string
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	LastLoggedIn time.Time
+}
 
+type userPublicDetails []userPublicDetail
+
+var listUsersHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//writeMessage("Hey you made it here", w)
+	userDetails, err := fetchUserDetails()
+	if err != nil {
+		log.Println(err.Error())
+		writeError(http.StatusInternalServerError, "Could not get user data", w)
+		return
+	}
+	reply, _ := json.Marshal(map[string]userPublicDetails{"message": userDetails})
+	w.Write(reply)
 })
+
+func fetchUserDetails() ([]userPublicDetail, error) {
+	rows, err := db.Query("SELECT username, name, createdAt, updatedAt, lastLoggedIn from app_user")
+	if err != nil {
+		return nil, errors.New("Cannot fetch user details: " + err.Error())
+	}
+	defer rows.Close() //make sure this is after checking for an error, or this will be a nil pointer dereference
+
+	var numUsers int
+	err = db.QueryRow("SELECT count(*) from app_user").Scan(&numUsers)
+
+	if err != nil {
+		return nil, errors.New("Cannot fetch user count: " + err.Error())
+	}
+
+	users := make([]userPublicDetail, numUsers)
+
+	index := 0
+	for thereAreMore := rows.Next(); thereAreMore; thereAreMore = rows.Next() {
+		var username, name string
+		var createdAt, updatedAt time.Time
+		var lastLoggedIn time.Time
+		err = rows.Scan(&username, &name, &createdAt, &updatedAt, &lastLoggedIn)
+		if err != nil {
+			return nil, errors.New("Could not extract user details: " + err.Error())
+		}
+		users[index] = userPublicDetail{username, name, createdAt, updatedAt, lastLoggedIn}
+		index++
+	}
+	return users, nil
+}
