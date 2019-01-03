efc7b1bd1b243677012b5805eb3dfa347f48c3cb
Ashwin <h1210093@nushigh.edu.sg>
2019-01-03 17:09:18 +0800

    Changed AccessControl to allow for errors in the access restrictions

--- auth/accesscontrol.go
+++ auth/accesscontrol.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type AccessRestriction func(jwt.MapClaims, *http.Request) bool
+type AccessRestriction func(jwt.MapClaims, *http.Request) (bool, error)
 
 func AccessControl(canAccess AccessRestriction, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -28,22 +28,25 @@ func AccessControl(canAccess AccessRestriction, h http.Handler) http.Handler {
 
 		claims, _ := token.Claims.(jwt.MapClaims)
 		r.Header.Set(JWTUsername, claims[JWTUsername].(string))
-		if canAccess(claims, r) {
+		if ok, err := canAccess(claims, r); err != nil {
+			log.Println("Error in access control: " + err.Error())
+			response.WriteMessage(http.StatusInternalServerError, "Error in checking credentials", w)
+		} else if ok {
 			h.ServeHTTP(w, r)
 		} else {
-			response.WriteMessage(http.StatusUnauthorized, "Access Unauthorized", w)
+			response.WriteMessage(http.StatusForbidden, "Access Denied", w)
 		}
 	})
 }
 
-func NoRestriction(claims jwt.MapClaims, r *http.Request) bool {
-	return true
+func NoRestriction(claims jwt.MapClaims, r *http.Request) (bool, error) {
+	return true, nil
 }
 
-func IsAdmin(claims jwt.MapClaims, r *http.Request) bool {
-	return claims[JWTAdminStatus] == true
+func IsAdmin(claims jwt.MapClaims, r *http.Request) (bool, error) {
+	return (claims[JWTAdminStatus] == true), nil
 }
 
-func SpecificUserOrAdmin(claims jwt.MapClaims, r *http.Request) bool {
-	return claims[JWTAdminStatus] == true || claims[JWTUsername] == mux.Vars(r)["username"]
+func SpecificUserOrAdmin(claims jwt.MapClaims, r *http.Request) (bool, error) {
+	return (claims[JWTAdminStatus] == true || claims[JWTUsername] == mux.Vars(r)["username"]), nil
 }
