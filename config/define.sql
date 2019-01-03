edca7766794e23cdc71febc417678ff5d3308508
Ashwin <h1210093@nushigh.edu.sg>
2019-01-03 14:51:13 +0800

    Added url field to event

--- config/define.sql
+++ config/define.sql
@@ -20,6 +20,7 @@ create table app_admin(
 create table event(
 	ID UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
 	name text NOT NULL,
+	url text UNIQUE NOT NULL,
 	start TIMESTAMP,
 	end TIMESTAMP,
 	lat float8,
@@ -43,18 +44,18 @@ INSERT INTO app_user (username, passwordHash, name, createdAt, updatedAt, lastLo
 INSERT into app_admin(username, passwordHash, name) VALUES 
 	('Hackerman','$2a$05$YNWHk.7Su/St644J1BAX7.G8KP3t5ts16bcAPApXSw.yc4hHrgwNi','Drop Table'); --BlackFireFoodHorse
 
-INSERT into event(name) VALUES
-	('Data Science CoP'),
-	('SDB Cohesion');
+INSERT into event(name, url) VALUES
+	('Data Science CoP','cop2018'),
+	('SDB Cohesion','sdbcohesionnovember');
 
-INSERT into event(name, start, "end", lat, long, radius) VALUES
-	('Data Science Department Talk', '2019-01-10 15:00:00', '2019-01-10 18:00:00', 1.335932, 103.744708, 0.5);
+INSERT into event(name, url, start, "end", lat, long, radius) VALUES
+	('Data Science Department Talk', 'dsdjan2019', '2019-01-10 15:00:00', '2019-01-10 18:00:00', 1.335932, 103.744708, 0.5);
 
 INSERT into hosts(username, eventID) VALUES
-	('ME5Bob', '6a148e7d-54d6-4b93-b008-64bb9abb624a'),
-	('TestUser', '3a09e470-50c8-467f-a2d8-98ac9a1d6f46'),
-	('TestUser', 'b0b60860-31e9-4395-bf7b-f0316f2cb86d'),
-	('ME5Bob', 'b0b60860-31e9-4395-bf7b-f0316f2cb86d');
+	('ME5Bob', '503144f1-7ab9-411a-94a0-c87a46e7102d'),
+	('TestUser', 'ffba6f26-b384-4217-8a96-42abd9cb6c4d'),
+	('TestUser', '3eb1f3a0-3937-4772-8804-02317157a00a'),
+	('ME5Bob', '3eb1f3a0-3937-4772-8804-02317157a00a');
 
 create USER server_access with password 'LongNightShortDay';
 grant CONNECT on DATABASE registrationapp to server_access;
