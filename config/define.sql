a69e532124bf89ff1af71927aa5f007ae03ca383
SKAshwin <h1210093@nushigh.edu.sg>
2019-01-31 14:25:50 +0800

    Fixed errors in .sql

--- config/define.sql
+++ config/define.sql
@@ -21,8 +21,8 @@ create table event(
 	ID UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
 	name text NOT NULL,
 	url text UNIQUE NOT NULL,
-	start TIMESTAMP,
-	end TIMESTAMP,
+	"start" TIMESTAMP,
+	"end" TIMESTAMP,
 	lat float8,
 	long float8,
 	radius float8, --in km
@@ -36,28 +36,28 @@ create table hosts(
 	PRIMARY KEY(username, eventID)
 );
 
-INSERT INTO app_user (username, passwordHash, name, createdAt, updatedAt, lastLoggedIn) VALUES
-	('ME5Bob', '$2a$05$xuLyzrAaW7Y4mXGAXzPjOOIdTim2BVGzePV73H.Vsy8gKCUxXqRB2', 'Robert Lim', NOW(), NOW(), NULL), --DarkHorseBigCat
+INSERT INTO app_user (username, passwordHash, name, createdAt, updatedAt, lastLoggedIn) VALUES --DONT PUT TAB AFTER VALUES, IT WILL FUCK UP ON LINUX BY ADDING EXTRA (
+    ('ME5Bob', '$2a$05$xuLyzrAaW7Y4mXGAXzPjOOIdTim2BVGzePV73H.Vsy8gKCUxXqRB2', 'Robert Lim', NOW(), NOW(), NULL), --DarkHorseBigCat
 	('ME6Alice', '$2a$05$KlEAswrxBhrgXr1Fh7Io0ecBHB182FeJAuP8BppK8BVRujEPlavkK', 'Alice Ng', NOW(), NOW(), NULL), --abcdef12345!@#$%
 	('safosscholar', '$2a$05$a6TJMr8YD4jUese5NKOL0.bDNvNMgV5.aDEhWpomDCYtz6jLn7GNe', 'Jonathan Tan', NOW(), NOW(), NOW()), --pas5W0rD!!!
 	('AirForceMan', '$2a$05$hAzW2PGlX4ig/oUChUxyA.1R0QqrKMCuxnlifS9qFbQQCivy6OvSq', 'Benjamin Net', NOW(), NOW(), NOW()), --WhyNotUseAnEasyPasswordLikeThis?
 	('TestUser', '$2a$05$D/nbFy9utEDFgg.Jfsl39epqO2Yx2nIRClYFGVMw9fnLZlXFFnP5u', 'User McUserson', NOW(), NOW(), NULL); --WhatAreDictionaryAttacks
 
 INSERT into app_admin(username, passwordHash, name) VALUES 
-	('Hackerman','$2a$05$YNWHk.7Su/St644J1BAX7.G8KP3t5ts16bcAPApXSw.yc4hHrgwNi','Drop Table'); --BlackFireFoodHorse
+    ('Hackerman','$2a$05$YNWHk.7Su/St644J1BAX7.G8KP3t5ts16bcAPApXSw.yc4hHrgwNi','Drop Table'); --BlackFireFoodHorse
 
 INSERT into event(name, url) VALUES
-	('Data Science CoP','cop2018'),
+    ('Data Science CoP','cop2018'),
 	('SDB Cohesion','sdbcohesionnovember');
 
 INSERT into event(name, url, start, "end", lat, long, radius) VALUES
-	('Data Science Department Talk', 'dsdjan2019', '2019-01-10 15:00:00', '2019-01-10 18:00:00', 1.335932, 103.744708, 0.5);
+    ('Data Science Department Talk', 'dsdjan2019', '2019-01-10 15:00:00', '2019-01-10 18:00:00', 1.335932, 103.744708, 0.5);
 
 INSERT into hosts(username, eventID) VALUES
-	('ME5Bob', '503144f1-7ab9-411a-94a0-c87a46e7102d'),
-	('TestUser', 'ffba6f26-b384-4217-8a96-42abd9cb6c4d'),
-	('TestUser', '3eb1f3a0-3937-4772-8804-02317157a00a'),
-	('ME5Bob', '3eb1f3a0-3937-4772-8804-02317157a00a');
+    ('ME5Bob', '2c59b54d-3422-4bdb-824c-4125775b44c8'),
+	('TestUser', '3820a980-a207-4738-b82b-45808fe7aba8'),
+	('TestUser', 'aa19239f-f9f5-4935-b1f7-0edfdceabba7'),
+	('ME5Bob', 'aa19239f-f9f5-4935-b1f7-0edfdceabba7');
 
 create USER server_access with password 'LongNightShortDay';
 grant CONNECT on DATABASE registrationapp to server_access;
