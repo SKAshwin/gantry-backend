c082c69d41ce70bcca0802e1b29e11f9209b4542
SKAshwin <h1210093@nushigh.edu.sg>
2019-01-31 14:30:09 +0800

    Added api versioning

--- go.mod
+++ go.mod
@@ -10,5 +10,5 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.0.0
 	github.com/rs/cors v1.6.0
-	golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9
+	golang.org/x/crypto v0.0.0-20190130090550-b01c7a725664
 )
--- routing/routing.go
+++ routing/routing.go
@@ -13,21 +13,21 @@ import (
 //SetUp Returns a router configured with all the handlers for each endpoint
 func SetUp() *mux.Router {
 	r := mux.NewRouter()
-	r.Handle("/api/admins/auth/login", handlers.AdminLogin).Methods("POST")
-	r.Handle("/api/users/auth/login", handlers.UserLogin).Methods("POST")
+	r.Handle("/api/v0/admins/auth/login", handlers.AdminLogin).Methods("POST")
+	r.Handle("/api/v0/users/auth/login", handlers.UserLogin).Methods("POST")
 
-	r.Handle("/api/users", Validate(handlers.ListUsers, IsAdmin)).Methods("GET")
-	r.Handle("/api/users", Validate(handlers.CreateUser, IsAdmin)).Methods("POST")
-	r.Handle("/api/users/{username}", handlers.UserExists(Validate(handlers.GetUser, IsSpecifiedUser, IsAdmin))).Methods("GET")
-	r.Handle("/api/users/{username}", handlers.UserExists(Validate(handlers.UpdateUser, IsSpecifiedUser, IsAdmin))).Methods("PUT")
-	r.Handle("/api/users/{username}", handlers.UserExists(Validate(handlers.DeleteUser, IsSpecifiedUser, IsAdmin))).Methods("DELETE")
+	r.Handle("/api/v0/users", Validate(handlers.ListUsers, IsAdmin)).Methods("GET")
+	r.Handle("/api/v0/users", Validate(handlers.CreateUser, IsAdmin)).Methods("POST")
+	r.Handle("/api/v0/users/{username}", handlers.UserExists(Validate(handlers.GetUser, IsSpecifiedUser, IsAdmin))).Methods("GET")
+	r.Handle("/api/v0/users/{username}", handlers.UserExists(Validate(handlers.UpdateUser, IsSpecifiedUser, IsAdmin))).Methods("PUT")
+	r.Handle("/api/v0/users/{username}", handlers.UserExists(Validate(handlers.DeleteUser, IsSpecifiedUser, IsAdmin))).Methods("DELETE")
 
-	r.Handle("/api/events", Validate(handlers.GetUsersEvents)).Methods("GET")
-	r.Handle("/api/events", Validate(handlers.CreateEvent)).Methods("POST")
-	r.Handle("/api/events/exists/{eventURL}", Validate(handlers.EventURLAvailable)).Methods("GET")
-	r.Handle("/api/events/{eventID}", handlers.EventExists(Validate(handlers.GetEvent, IsHost, IsAdmin))).Methods("GET")
-	r.Handle("/api/events/{eventID}", handlers.EventExists(Validate(handlers.UpdateEvent, IsHost, IsAdmin))).Methods("PUT")
-	r.Handle("/api/events/{eventID}", handlers.EventExists(Validate(handlers.DeleteEvent, IsHost, IsAdmin))).Methods("DELETE")
+	r.Handle("/api/v0/events", Validate(handlers.GetUsersEvents)).Methods("GET")
+	r.Handle("/api/v0/events", Validate(handlers.CreateEvent)).Methods("POST")
+	r.Handle("/api/v0/events/exists/{eventURL}", Validate(handlers.EventURLAvailable)).Methods("GET")
+	r.Handle("/api/v0/events/{eventID}", handlers.EventExists(Validate(handlers.GetEvent, IsHost, IsAdmin))).Methods("GET")
+	r.Handle("/api/v0/events/{eventID}", handlers.EventExists(Validate(handlers.UpdateEvent, IsHost, IsAdmin))).Methods("PUT")
+	r.Handle("/api/v0/events/{eventID}", handlers.EventExists(Validate(handlers.DeleteEvent, IsHost, IsAdmin))).Methods("DELETE")
 
 	return r
 }
