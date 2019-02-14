33596b5375681b61e9e9411463d3c807ecd28047
SKAshwin <h1210093@nushigh.edu.sg>
2019-02-14 16:41:24 +0800

    SHA512 now returns hex representation of byte output

--- sha512/sha512.go
+++ sha512/sha512.go
@@ -2,6 +2,7 @@ package sha512
 
 import (
 	"crypto/sha512"
+	"encoding/hex"
 )
 
 //HashMethod a SHA512 implementation of HashMethod
@@ -9,17 +10,17 @@ type HashMethod struct {
 }
 
 //HashAndSalt Hashes a function using SHA512, will never
-//return an error
-//no salt
+//return an error. No salt.
+//Returns a hex encoding of the SHA512 byte output
 func (hm HashMethod) HashAndSalt(pwd string) (string, error) {
 	bytes := sha512.Sum512([]byte(pwd)) //needs to be two lines
 	//cant call [:] on a non-addressable value
-	return string(bytes[:]), nil
+	return hex.EncodeToString(bytes[:]), nil
 }
 
 //CompareHashAndPassword Returns true if the hashed string matches the plaintext
 //Returns false otherwise
 func (hm HashMethod) CompareHashAndPassword(hash string, pwd string) bool {
 	bytes := sha512.Sum512([]byte(pwd))
-	return hash == string(bytes[:])
+	return hash == hex.EncodeToString(bytes[:])
 }
