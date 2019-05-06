f1fbb190e3becd4447c865208c6f01fef510fcb3
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2019-05-06 13:48:51 +0800

    Added multiple connections per listener support

--- http/gorillawebsocket/websocket.go
+++ http/gorillawebsocket/websocket.go
@@ -12,7 +12,7 @@ import (
 //GuestMessenger is an implementation of http.GuestMessenger which uses gorilla/websocket to set up
 //a websocket connection with a guest, and allows for communication with the guest
 type GuestMessenger struct {
-	connections map[string]*GuestConnection
+	connections map[string][]*GuestConnection
 	upgrader    websocket.Upgrader
 	lock        sync.RWMutex
 }
@@ -22,7 +22,7 @@ type GuestMessenger struct {
 //Set both to 1024 if you don't know what you want
 func NewGuestMessenger(readBufferSize int, writeBufferSize int) *GuestMessenger {
 	return &GuestMessenger{
-		connections: make(map[string]*GuestConnection),
+		connections: make(map[string][]*GuestConnection),
 		upgrader: websocket.Upgrader{
 			ReadBufferSize:  readBufferSize,
 			WriteBufferSize: writeBufferSize,
@@ -38,42 +38,64 @@ func NewGuestMessenger(readBufferSize int, writeBufferSize int) *GuestMessenger 
 //OpenConnection opens a websocket connection with the guest, saving that connection under the given
 //guest ID, given a responsewriter and request from said guest.
 func (gm *GuestMessenger) OpenConnection(guestID string, w http.ResponseWriter, r *http.Request) error {
+	gm.lock.Lock()
+	defer gm.lock.Unlock()
 	conn, err := gm.upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return errors.New("Error upgrading request to websocket connection: " + err.Error())
 	}
 
-	gm.lock.Lock()
-	defer gm.lock.Unlock()
-	gm.connections[guestID] = newGuestConnection(conn, func() {
+	newConn := newGuestConnection(conn, func() {
 		gm.lock.Lock()
 		defer gm.lock.Unlock()
 		delete(gm.connections, guestID)
 	})
 
+	if conns, ok := gm.connections[guestID]; !ok {
+		//initialize it if it does not exist
+		conns := make([]*GuestConnection, 1, 10)
+		conns[0] = newConn
+		gm.connections[guestID] = conns
+	} else {
+		gm.connections[guestID] = append(conns, newConn)
+	}
+
 	return nil
 }
 
-//Send sends the provided guest data over the websocket connection marked by the given guestID
+//Send sends the provided guest data over the websocket connections marked by the given guestID
 //Must have called OpenConnection with that guestID beforehand
 func (gm *GuestMessenger) Send(guestID string, data myhttp.GuestMessage) error {
 	gm.lock.RLock()
 	defer gm.lock.RUnlock()
-	if client, ok := gm.connections[guestID]; ok {
+	if clients, ok := gm.connections[guestID]; ok {
 		response := make(chan error)
-		client.send <- SendTask{message: data, response: response}
-		err := <-response
-		if err != nil {
-			return errors.New("Error writing message: " + err.Error())
+		errorList := make([]error, len(clients))
+		anyerr := false
+		for i, client := range clients {
+			client.send <- SendTask{message: data, response: response}
+			errorList[i] = <-response
+			if errorList[i] != nil {
+				anyerr = true
+			}
+		}
+
+		if anyerr {
+			errString := "Error(s) occured when sending message: "
+			for _, err := range errorList {
+				if err != nil {
+					errString += err.Error() + "\n"
+				}
+			}
+			return errors.New(errString)
 		}
 
 		return nil
 	}
-
 	return errors.New("No such guest ID")
 }
 
-//HasConnection returns true if there is an active connection with the given guest ID
+//HasConnection returns true if there is at least one active connection with the given guest ID
 func (gm *GuestMessenger) HasConnection(guestID string) bool {
 	gm.lock.RLock()
 	defer gm.lock.RUnlock()
@@ -81,12 +103,14 @@ func (gm *GuestMessenger) HasConnection(guestID string) bool {
 	return ok
 }
 
-//CloseConnection closes the websocket connection marked with the given guestID
+//CloseConnection closes ALL the websocket connections marked with the given guestID
 func (gm *GuestMessenger) CloseConnection(guestID string) error {
-	if client, ok := gm.connections[guestID]; ok {
-		client.close()
-		gm.lock.Lock()
-		defer gm.lock.Unlock()
+	gm.lock.Lock()
+	defer gm.lock.Unlock()
+	if clients, ok := gm.connections[guestID]; ok {
+		for _, client := range clients {
+			client.close()
+		}
 		delete(gm.connections, guestID)
 		return nil
 	}
--- http/gorillawebsocket/websocket_test.go
+++ http/gorillawebsocket/websocket_test.go
@@ -8,8 +8,9 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"strings"
-	"time"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -72,7 +73,7 @@ func TestGuestMessenger(t *testing.T) {
 	//should not longer be listed as an active connection
 	//given sufficient delay first
 	ws.Close()
-	time.Sleep(25*time.Millisecond)
+	time.Sleep(25 * time.Millisecond)
 	test.Assert(t, !gm.HasConnection("1234"), "1234 still listed as a connection even though the websocket was closed by the other end")
 
 	//Try the other connection
@@ -84,11 +85,62 @@ func TestGuestMessenger(t *testing.T) {
 
 	//See what happens if you close the connection from the other end gracefully
 	ws2.WriteMessage(websocket.CloseMessage, []byte{})
-	time.Sleep(25*time.Millisecond)
+	time.Sleep(25 * time.Millisecond)
 	test.Assert(t, !gm.HasConnection("3000"), "1234 still listed as a connection even though the websocket was closed by the other end")
 
 	//shouldn't be able to close a connection that's already closed
 	err = gm.CloseConnection("3000")
 	test.Assert(t, err != nil, "Attempting to close already closed connection failed to throw an error")
 
+}
+
+func TestSendMultipleConnections(t *testing.T) {
+	gm := mywebsocket.NewGuestMessenger(2048, 2048)
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := gm.OpenConnection(r.Header.Get("GuestID"), w, r)
+		test.Ok(t, err)
+	}))
+	defer s.Close()
+
+	type msg struct {
+		Title   string        `json:"title"`
+		Content checkin.Guest `json:"content"`
+	}
+
+	url := "ws" + strings.TrimPrefix(s.URL, "http")
+	header1 := make(http.Header)
+	header1.Add("GuestID", "1234")
+	header2 := make(http.Header)
+	header2.Add("GuestID", "2234")
+	var wg sync.WaitGroup
+	var reading sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		reading.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			var guest msg
+			if n%2 == 0 {
+				ws, _, err := websocket.DefaultDialer.Dial(url, header1)
+				test.Ok(t, err)
+				reading.Done()
+				err = ws.ReadJSON(&guest)
+				test.Ok(t, err)
+				test.Equals(t, msg{Title: "Check in", Content: checkin.Guest{NRIC: "1234", Name: "Jim Bob"}}, guest)
+			} else {
+				ws, _, err := websocket.DefaultDialer.Dial(url, header2)
+				test.Ok(t, err)
+				reading.Done()
+				err = ws.ReadJSON(&guest)
+				test.Ok(t, err)
+				test.Equals(t, msg{Title: "Check in", Content: checkin.Guest{NRIC: "2234", Name: "Jimothy Bob"}}, guest)
+			}
+
+		}(i)
+	}
+	reading.Wait()
+	gm.Send("1234", myhttp.GuestMessage{Title: "Check in", Content: checkin.Guest{NRIC: "1234", Name: "Jim Bob"}})
+	gm.Send("2234", myhttp.GuestMessage{Title: "Check in", Content: checkin.Guest{NRIC: "2234", Name: "Jimothy Bob"}})
+	wg.Wait()
 }
