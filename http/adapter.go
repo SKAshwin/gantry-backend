7d42a50898877d8718d42abe70608215ec14b2db
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2019-06-10 15:50:53 +0800

    Implemented middleware to set timezone of request body times using ?loc query param

--- http/adapter.go
+++ http/adapter.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -39,20 +40,41 @@ func correctTimezonesOutput(h http.Handler) http.Handler {
 	})
 }
 
-/*
+//Middleware which intercepts the request, assuming that its body is in a JSON format, and parses it to find any strings that are meant to be times
+//and then ensures that the time is interpreted to be of the timezone that is provided in the ?loc form query parameter
+//Timezone must follow the IANA time zone database names
+//If no ?loc argument is given, times will be interpreted as UTC
 func correctTimezonesInput(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		locationName := r.FormValue("loc")
+		locationName := r.FormValue("loc") //no loc will return "", which will be parsed as UTC
 		location, err := time.LoadLocation(locationName)
 		if err != nil {
 			WriteMessage(http.StatusBadRequest, "Could not parse location "+locationName+" in form argument loc. Use IANA time zone database names", w)
 			return
 		}
+		res, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Could not read request body in correct timezone middleware: " + err.Error())
+			WriteMessage(http.StatusBadRequest, "Could not read request body to correct timezones", w)
+			return
+		}
+		json := string(res)
+
+		newJSON := correctJSONTimeZone(json, func(old time.Time) time.Time {
+			if old.IsZero() {
+				return old
+			}
+			return time.Date(old.Year(), old.Month(), old.Day(), old.Hour(), old.Minute(), old.Second(), old.Nanosecond(), location)
+		})
+
+		r.Body = ioutil.NopCloser(strings.NewReader(newJSON))
+		r.ContentLength = int64(len(newJSON))
 
 		h.ServeHTTP(w, r)
 	})
-}*/
+}
 
+//Writer which adjusts timezones being written to it before sending it to the client
 type timeZoneAdjustedWriter struct {
 	w          http.ResponseWriter
 	loc        *time.Location
