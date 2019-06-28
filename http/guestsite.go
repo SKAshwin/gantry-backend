f6f92ee3342f24546cc015b0269ef0adb258497d
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2019-06-28 09:10:26 +0800

    Added middleware checking if a guest site exists

--- http/guestsite.go
+++ http/guestsite.go
@@ -38,20 +38,38 @@ func NewGuestSiteHandler(gss checkin.GuestSiteService, es checkin.EventService, 
 	//Adapters to check if handler should serve the request
 	tokenCheck := checkAuth(auth, h.Logger)
 	credentialsCheck := isAdminOrHost(auth, es, "eventID", h.Logger)
-	existCheck := eventExists(es, "eventID", h.Logger)
+	eventExistCheck := eventExists(es, "eventID", h.Logger)
 	urlConversion := eventURLToID(es, "eventID", h.Logger)
+	siteExistCheck := websiteExists(gss, "eventID", h.Logger)
 	h.Handle("/api/v1-4/events/{eventID}/website", Adapt(http.NotFoundHandler(),
-		urlConversion, existCheck)).Methods("GET") //GET the website JSON
+		urlConversion, eventExistCheck, siteExistCheck)).Methods("GET") //GET the website JSON
 	h.Handle("/api/v1-4/events/{eventID}/website", Adapt(http.NotFoundHandler(),
-		urlConversion, existCheck, tokenCheck, credentialsCheck)).Methods("POST") //create a new website for that event
+		urlConversion, eventExistCheck, tokenCheck, credentialsCheck)).Methods("POST") //create a new website for that event
 	h.Handle("/api/v1-4/events/{eventID}/website", Adapt(http.NotFoundHandler(),
-		urlConversion, existCheck, tokenCheck, credentialsCheck)).Methods("PATCH") //update the website's JSON
+		urlConversion, eventExistCheck, siteExistCheck, tokenCheck, credentialsCheck)).Methods("PATCH") //update the website's JSON
 	h.Handle("/api/v1-4/events/{eventID}/website", Adapt(http.NotFoundHandler(),
-		urlConversion, existCheck, tokenCheck, credentialsCheck)).Methods("DELETE") //remove the website's JSON
+		urlConversion, eventExistCheck, siteExistCheck, tokenCheck, credentialsCheck)).Methods("DELETE") //remove the website's JSON
 
 	return h
 }
 
 func (h *GuestSiteHandler) handleWebsite(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func websiteExists(gss checkin.GuestSiteService, eventIDKey string, logger *log.Logger) Adapter {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			eventID := mux.Vars(r)[eventIDKey]
+			ok, err := gss.GuestSiteExists(eventID)
+			if err != nil {
+				logger.Println("Error checking that guest site exists: " + err.Error())
+				WriteMessage(http.StatusInternalServerError, "Error checking if guest site exists", w)
+			} else if ok {
+				h.ServeHTTP(w, r)
+			} else {
+				WriteMessage(http.StatusNotFound, "Guest site does not exist for event with that ID", w)
+			}
+		})
+	}
 }
